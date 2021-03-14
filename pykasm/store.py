5a9b0f6d38936bc358b540533c5794351edf504a
Alexander Reynolds <alex@theory.shop>
2021-03-14 00:26:52 -0800

    Revamp project
    Move to 1.0
    Remove unnecessary deps
    Remove timeout iterable (can use queue instead)
    Remove stream buffer (can use queue or store instead)
    Add unit tests to store and queues
    Add type checking

--- pykasm/store.py
+++ pykasm/store.py
@@ -1,60 +1,48 @@
-from datetime import datetime
-from collections import defaultdict
-from concurrent.futures import ThreadPoolExecutor
+import collections
+import concurrent.futures
+import logging
+import threading
 from typing import Any, Callable, DefaultDict, Dict
-from uuid import uuid4
-
-Callback = Callable[[datetime, Any], Any]
+import uuid
 
 
-class Writer:
-    """A writer for a specific key in the Store.
+logger = logging.getLogger(__name__)
 
-    Not meant to be instantiated directly, but via `writer = store.writer(key)`.
-    """
-
-    def __init__(self, key: Any, submit: Callback):
-        self.key = key
-        self.submit: Callback = submit
-
-    def write(self, value: Any):
-        """Writes a value to the store, executing any registered callbacks.
-
-        Args:
-            value: Value to write to the store.
-        """
-        self.submit(datetime.now(), value)
-
-    def __repr__(self):
-        return f"<{self.__class__.__qualname__} for key {self.key!r}>"
+Callback = Callable[[Any], Any]
 
 
 class Store:
     """Store for arbitrary data with an executor for submitting callback tasks.
 
-    You can get a writer for a `key` with `writer(key)`. Only one writer can be
-    created per `key` on each store.
+    Register callbacks and create writers associated to a key; when a writer
+    writes to the store, it will submit tasks to the executor to invoke all the
+    callbacks for that key with the newly written value.
 
-    The callback functions provided should take two positional arguments, the first
-    being a `datetime` when the data was added to the store, and the second being the
-    value added to the store.
+    Retrieve a writer for some `key` with `store.writer(key)`.
+
+    The callback functions provided must take a single positional argument,
+    and will be called with the new value added to the store.
 
     .. code-block:: python
 
-        store = Store(ThreadPoolExecutor())
+        executor = ThreadPoolExecutor()
+        store = Store(executor)
 
         # q holds a queue of data, d holds the most recent value
         q = deque()
         d = {'value': None}
 
-        q_name = store.add_callback("data", lambda ts, val: q.append(val), "q")
-        store.add_callback("data", lambda ts, val: d.update(value=val))
-        # name not used, so return value ignored
+        # store a reference to the callback to remove later
+        q_id = store.add_callback("data", q.append)
+
+        # reference not needed since this callback won't be removed
+        store.add_callback("data", lambda val: d.update(value=val))
 
         writer = store.writer("data")
         for i in range(5):
             writer.write(i)
-        store.remove_callback("data", q_name)
+
+        store.remove_callback(q_id)
         writer.write(5)  # value of 5 was only sent to the unnamed callback
 
         print("q", q)
@@ -66,69 +54,117 @@ class Store:
         {'value': 5}
     """
 
-    def __init__(self, executor: ThreadPoolExecutor):
-        self.executor: ThreadPoolExecutor = executor
-        self.writers: Dict[Any, Writer] = {}
-        self.callbacks: DefaultDict[Any, Dict[str, Callback]] = defaultdict(dict)
+    executor: concurrent.futures.ThreadPoolExecutor
+    writers: Dict[Any, "Writer"]
+    callbacks: DefaultDict[Any, Dict[str, Callback]]
+    _callback_keys: Dict[str, Any]
+    _callback_lock: threading.Lock
 
-    def _submitter(self, key: Any) -> Callback:
-        def submit(ts: datetime, val: Any):
-            for f in self.callbacks[key].values():
-                self.executor.submit(f, ts, val)
-
-        return submit
+    def __init__(self, executor: concurrent.futures.ThreadPoolExecutor):
+        self.executor = executor
+        self.writers = {}
+        self.callbacks = collections.defaultdict(dict)
+        self._callback_keys = {}
+        self._callback_lock = threading.Lock()
 
     def add_callback(self, key: Any, f: Callback) -> str:
         """Add a callback to execute on new values written under the key.
 
         The returned value is a string id for the callback which can be used to
-        remove it later with `remove_callback()`.
+        remove it later with `remove_callback(id)`.
 
         Args:
             key: The key which links the callback to the writer. The key can be
                 any immutable type (same restriction as a dictionary key).
-            f: Callable that will be executed as f(datetime, value) for each new
-                value written to the store under this key. The return value will
-                be discarded.
+            f: Callable executed as f(value) for each new value written to the
+                store under this key. The return value is discarded. Exceptions
+                encountered during execution are logged, then ignored.
 
         Returns:
-            str: An id for the callback, which can be used for unregistering the callback.
-                The string is in the form "<func_name>::<key>::<hex-uuid>"
+            str: An id for the callback, which can be used for unregistering
+                the callback. The id has the form "<key>::<f.__name__>::<uuid>".
         """
-        id = f"{f.__name__}::{key}::{uuid4().hex}"
-        self.callbacks[key][id] = f
+        id = f"{key}::{f.__name__}::{uuid.uuid4().hex}"
+        with self._callback_lock:
+            self.callbacks[key][id] = f
+            self._callback_keys[id] = key
         return id
 
-    def remove_callback(self, key: Any, id: str):
-        """Removes a callback for the specified (key, id) pair.
+    def remove_callback(self, id: str) -> None:
+        """Removes the specified callback.
 
         Raises:
-            KeyError: If there aren't any callbacks associated with the (key, id) pair.
+            KeyError: If there is no callback with the given id.
 
         Args:
-            key: The key the callback is registered under.
-            id: The unique identifier for the callback, returned by `add_callback()`
+            id: The identifier for the callback, returned by `add_callback()`.
         """
-        del self.callbacks[key][id]
+        with self._callback_lock:
+            key = self._callback_keys[id]
+            del self._callback_keys[id]
+            del self.callbacks[key][id]
 
-    def writer(self, key: Any) -> Writer:
-        """Creates a writer for the specified key.
+    def writer(self, key: Any) -> "Writer":
+        """Creates or retrieves a writer for the specified key.
 
         Args:
             key: A key to associate writers and callbacks. Any dictionary key
                 is valid.
 
-        Raises:
-            ValueError: If a writer already exists for this key.
-
         Returns:
             A writer for the specified key.
         """
-        if key in self.writers:
-            raise ValueError(f"Already created a writer for key {key}")
-        w = Writer(key, self._submitter(key))
-        self.writers[key] = w
-        return w
+        try:
+            return self.writers[key]
+        except KeyError:
+            w = Writer(key, self._submitter(key))
+            self.writers[key] = w
+            return w
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"<{self.__class__.__qualname__} for keys {*(k for k in self.writers),}>"
+
+    def _submitter(self, key: Any) -> Callback:
+        def submit(val: Any) -> Any:
+            with self._callback_lock:
+                callbacks = list(self.callbacks[key].values())
+            for callback in callbacks:
+                future = self.executor.submit(callback, val)
+                future.add_done_callback(_log_exception(key))
+
+        return submit
+
+
+class Writer:
+    """A writer for a specific key in the Store.
+
+    Should be instantiated via `writer = store.writer(key)`.
+    """
+
+    def __init__(self, key: Any, submit: Callback):
+        self.key = key
+        self.submit: Callback = submit
+
+    def write(self, value: Any) -> None:
+        """Writes a value to the store, executing any registered callbacks.
+
+        Args:
+            value: Value to write to the store.
+        """
+        self.submit(value)
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__qualname__} for key {self.key!r}>"
+
+
+def _log_exception(key: Any) -> Callable[[concurrent.futures.Future], None]:
+    def f(future: concurrent.futures.Future) -> None:
+        e = future.exception()
+        if e is not None:
+            logger.error(
+                "Writer for key '%s' encountered an exception during callback execution:",
+                key,
+                exc_info=e,
+            )
+
+    return f
---
+++ tests/test_store.py
@@ -0,0 +1,97 @@
+import pytest
+import pykasm.store
+import concurrent.futures
+
+
+@pytest.fixture
+def store():
+    return pykasm.store.Store(concurrent.futures.ThreadPoolExecutor())
+
+
+def test_writer():
+    l = []
+    w = pykasm.store.Writer("key", l.append)
+    w.write(1)
+    assert l == [1]
+    w.write("2")
+    assert l == [1, "2"]
+
+
+def test_store_writer(store):
+    l = []
+    w = store.writer("key")
+    store.add_callback("key", l.append)
+    w.write(1)
+    assert l == [1]
+    w.write("2")
+    assert l == [1, "2"]
+
+
+def test_multiple_writers(store):
+    l = []
+    w1 = store.writer("key")
+    w2 = store.writer("key")
+    store.add_callback("key", l.append)
+    w1.write(1)
+    assert l == [1]
+    w2.write("2")
+    assert l == [1, "2"]
+
+
+def test_multiple_callbacks(store):
+    l = []
+    d = {"value": None}
+    w = store.writer("key")
+    store.add_callback("key", l.append)
+    store.add_callback("key", lambda v: d.update(value=v))
+    w.write(1)
+    assert l == [1]
+    assert d == {"value": 1}
+    w.write("2")
+    assert l == [1, "2"]
+    assert d == {"value": "2"}
+
+
+def test_multiple_callbacks_multiple_writers(store):
+    l = []
+    d = {"value": None}
+    w1 = store.writer("key")
+    w2 = store.writer("key")
+    store.add_callback("key", l.append)
+    store.add_callback("key", lambda v: d.update(value=v))
+    w1.write(1)
+    assert l == [1]
+    assert d == {"value": 1}
+    w2.write("2")
+    assert l == [1, "2"]
+    assert d == {"value": "2"}
+
+
+def test_remove_callback(store):
+    l = []
+    w = store.writer("key")
+    cb = store.add_callback("key", l.append)
+    w.write(1)
+    assert l == [1]
+    store.remove_callback(cb)
+    w.write(1)
+    assert l == [1]
+
+
+def test_remove_callback_multiple_times_errors(store):
+    w = store.writer("key")
+    cb = store.add_callback("key", lambda: None)
+    store.remove_callback(cb)
+    with pytest.raises(KeyError):
+        store.remove_callback(cb)
+
+
+def test_callback_error_logged(store, caplog):
+    l = []
+    w = store.writer("key")
+    store.add_callback("key", l.insert)  # insert requires 2 args
+    w.write(1)
+    assert [
+        "Writer for key 'key' encountered an exception during callback execution:"
+    ] == [rec.message for rec in caplog.records]
+    assert ["ERROR"] == [rec.levelname for rec in caplog.records]
